a1173839b63415c9f63a1fbdba84c8637b79bc28
TruongPX <truongpx@forlife.vn>
2022-01-26 13:38:36 +0700

    encrypt and decrypt data with defautl secret key

--- test.js
+++ test.js
@@ -8,19 +8,17 @@ const SECRET_KEY = "something_secret";
 const DB_CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}`;
 
 let conn = t.helper.connect_to_db(DB_CONNECTION_URL);
-let test = t.helper.encrypt_content("1w234", SECRET_KEY);
-console.log(t.helper.decrypt_content(test, SECRET_KEY));
 
-// (async () => {
-//     try {
-//         let user_coll = await t.user_coll.initialize_collection();
-//         let password = t.helper.hash_password("admin");
-//         console.log("after hash ", password);
-//         let data = await t.helper.insert_one(user_coll, { hey: 1, ez: "Trueeeeeeeeeeeeeeeeeeeeeeeeeeeee", user_name: "tatata", password: "lam j ma phai xoan" });
-//         console.log(data);
-//         process.exit(1);
-//     } catch (e) {
-//         // Deal with the fact the chain failed
-//         console.log(e);
-//     }
-// })();
+(async () => {
+    try {
+        let user_coll = await t.info_coll.initialize_collection();
+        let password = t.helper.hash_password("admin");
+        console.log("after hash ", password);
+        let data = await t.helper.insert_one(user_coll, { title: 1, content: "Trueeeeeeeeeeeeeeeeeeeeeeeeeeeee", user_id: "tatata", password: "lam j ma phai xoan" });
+        console.log(data);
+        process.exit(1);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
